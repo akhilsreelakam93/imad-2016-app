8b3ba3b10390b40d92438dd93ab9e2dd0562d159
Akhil Suresh <akhilsreelakam93@gmail.com>
2016-11-03 22:18:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bomb.png'));
